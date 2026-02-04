5f8d99428b0bcb39f96d68726c06dc314f3f00e3
chathubha <chathubhavithanagamage@gmail.com>
2026-02-04 16:24:58 +0530

    connected MongoDB Atlas + Mongoose

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
+// app.use(express.json());
+
+// app.get("/", (req, res) => {
+//   res.send("Expense Tracker API is running");
+// });
+
+// const PORT = 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
 app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
   res.send("Expense Tracker API is running");
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
