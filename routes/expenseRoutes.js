c77231aebb11e2f9409b407ed4af1126beec4848
chathubha <chathubhavithanagamage@gmail.com>
2026-02-05 15:06:50 +0530

    created update operation, now CRUD operation is finished

--- routes/expenseRoutes.js
+++ routes/expenseRoutes.js
@@ -30,6 +30,27 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.put("/:id", async (req, res) => {
+  try {
+    const { title, amount, category, date } = req.body;
+
+    const updatedExpense = await Expense.findByIdAndUpdate(
+      req.params.id,
+      { title, amount, category, date },
+      { new: true }
+    );
+
+    if (!updatedExpense) {
+      return res.status(404).json({ message: "Expense not found" });
+    }
+
+    res.status(200).json(updatedExpense);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+
 router.delete("/:id", async (req, res) => {
   try {
     const expense = await Expense.findByIdAndDelete(req.params.id);
